3f0d0920026ab97c95147e0e329410b50b10e9de
Gibson Han <order9066@Gibsons-Mac-mini.local>
2021-02-04 16:35:44 -0800

    add google login button with auth to firebase

--- src/screen/Account.tsx
+++ src/screen/Account.tsx
@@ -49,13 +49,42 @@ const Account: React.FC<{}> = (props) => {
         // ...
       });
   };
+  const handleSignInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    firebase.auth().languageCode = 'en';
+
+    firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+  }
   if (initializing) return null;
   if (!user) {
     return (
       <View style={styles.root}>
         {console.log(user)}
         <Text>Hello</Text>
+
         <Button title="Login" onPress={handleLogin} />
+        <Button title="Login With Google" onPress={handleSignInWithGoogle} />
       </View>
     );
   }
